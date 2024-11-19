cddd2db4b64cf3a0d30e932f0495011a1219df77
schaldach <gabimorgado0311@gmail.com>
2024-11-19 19:42:37 -0300

    Fazendo loop, pedindo comandos, inserindo dados na tabela

--- projetom3.sql
+++ projetom3.sql
@@ -12,7 +12,12 @@ CREATE TABLE Tecnico (
 );
 
 INSERT INTO Tecnico (cracha, senha) VALUES 
-(123456, "abcd1234");
+(123456, "abcd1234"),
+(100100, "efgh5678"),
+(500, "efgh1234"),
+(4000, "abcd5678");
+
+SELECT * FROM Tecnico;
 
 -- Troca(id, id_tecnico, motivo, protocolo, data_troca)
 -- DROP TABLE Troca;
@@ -25,12 +30,26 @@ CREATE TABLE Troca (
 	FOREIGN KEY (id_tecnico) REFERENCES Tecnico (id)
 );
 
+INSERT INTO Troca (id_tecnico, motivo, protocolo, data_troca) VALUES 
+(1, NULL, "abcdefgh124", "2024-11-18"),
+(2, "Não funcionou", "abcdefgh125", "2024-11-19");
+
+SELECT * FROM Troca;
+
 -- Status_maquina(id, nome)
 -- DROP TABLE Status_maquina;
 CREATE TABLE Status_maquina (
     id INT PRIMARY KEY auto_increment,
     nome VARCHAR(30) NOT NULL UNIQUE
 );
+
+INSERT INTO Status_maquina (nome) VALUES 
+("Funcionando"),
+("Trocado"),
+("Esperando troca"),
+("Não recebido");
+
+SELECT * FROM Status_maquina;
 
 -- Maquina(id, id_troca, id_status, numero_serie, numero_logico, modelo, data_recebimento)
 -- DROP TABLE Maquina;
@@ -45,3 +64,11 @@ CREATE TABLE Maquina (
     FOREIGN KEY (id_troca) REFERENCES Troca(id), 
     FOREIGN KEY (id_status) REFERENCES Status_maquina(id) 
 );
+
+INSERT INTO Maquina (id_troca, id_status, numero_serie, numero_logico, modelo, data_recebimento) VALUES 
+(NULL, 1, "123abc", 1234, "modelo123", "2024-11-07"),
+(1, 2, "123abd", 1235, "modelo124", "2024-11-10"),
+(2, 3, "123abe", 1236, "modelo125", "2024-11-11"),
+(2, 3, "123abf", 1237, "modelo126", "2024-11-12");
+
+SELECT * FROM Maquina;
--- script.py
+++ script.py
@@ -8,24 +8,93 @@ connection = mysql.connector.connect(
     user=input("Nome do usuário MySQL (admin): "),  # Your MySQL username
     password=input("Senha do usuário MySQL: "), # Your MySQL password
     database="controle_lius"  # Database name to connect to (optional)
-)
+)    
 
-if connection.is_connected():
-    print("Connected to MySQL database!")
+commands = [
+    {
+        "command":["SELECT * FROM "], 
+        "required":["Digite o nome da tabela: "]
+    },
+    {
+        "command":["SELECT * FROM ", " WHERE id = "], 
+        "required":["Digite o nome da tabela: ", "Digite o id do registro: "]
+    },
+    {
+        "command":["UPDATE "," SET ", " = "," WHERE id = "], 
+        "required":["Digite o nome da tabela: ", "Digite o nome da coluna: ", "Digite o novo valor da coluna", "Digite o id do registro: "]
+    },
+    {
+        "command":["DELETE FROM ", " WHERE id = "], 
+        "required":["Digite o nome da tabela: ", "Digite o id do registro: "]
+    },
+    {
+        "command":["INSERT INTO ", "(", ") VALUES "], 
+        "required":["Digite o nome da tabela: ", "Digite o nome dos campos a serem preenchidos, separados por vírgula (,): ", "Digite os valores a serem preenchidos, começando e terminando com \nparênteses, e separados por vírgula para múltiplas linhas: "]
+    }
+]
+
+def input_command_fields(choice_number):
+    full_command = ""
+    for index, command_text in enumerate(commands[choice_number]["command"]):
+        full_command += command_text
+        full_command += input(commands[choice_number]["required"][index])
+    full_command += ";"
+    return full_command
 
 try:
-    # precisaremos documentar cada comando feito pelo cursor em MySQL...
+    # o "cursor" neste caso não é equivalente ao cursor do MySQL, mas sim apenas uma abstração do Python
+    # para interagir com o MySQL e executar comandos
     cursor = connection.cursor()
 
-    # Execute a query
-    cursor.execute("SELECT * FROM Tecnico;")
-    
-    # Fetch results
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
+    while True:
+        print("\n-------")
+        print("Menu:")
+        print("1. Selecionar todos de uma tabela")
+        print("2. Selecionar linha de uma tabela")
+        print("3. Atualizar um campo de uma linha de uma tabela")
+        print("4. Deletar uma linha de uma tabela")
+        print("5. Inserir linha(s) em uma tabela")
+        print("6. Digitar comando SQL customizado")
+        print("7. Sair")
+        print("-------\n")
+        choice = int(input("Escolha uma opção: "))
+        
+
+        # se a escolha está entre 1 e 6
+        if choice in list(range(1,7)):
+            # se for 6, pedimos o comando, senão, montamos ele de acordo com os comandos pré-determinados
+            full_command = input_command_fields(choice-1) if choice<6 else input("Digite o comando SQL a ser executado: ")
+
+            try:
+                # executar comando retornado
+                cursor.execute(full_command)
+            except mysql.connector.Error as e:
+                print(f"Ocorreu o seguinte erro: {e}")
+                continue
+
+            # se foi feito um select, mostrar os resultados (iremos detectar de forma automática)
+            if full_command.strip().upper().startswith("SELECT"):
+                # fetchall() irá pegar todas as linhas resultadas do último SELECT executado
+                results = cursor.fetchall()
+                for row in results:
+                    print(row)
+            # senão, executar o comando
+            else:
+                # equivalente ao comando COMMIT; se estivesse em uma transação, o que irá 
+                # efetivamente executar o comando e alterar os dados na base de dados
+                connection.commit()
+            print("\nComando executado com sucesso!")
+
+        elif choice == 7:
+            print("Saindo...")
+            break
+
+        else:
+            print("Opção inválida, tente novamente")
+
+except mysql.connector.Error as e:
+    print("Ocorreu o seguinte erro: "+e)
 
 finally:
-    # Close cursor and connection
     cursor.close()
     connection.close()
